ff0ffe2607148b4bd0842ec268c5fe16c3e01901
Thibault Wittemberg <thibault.wittemberg@gmail.com>
2018-01-19 18:53:10 -0500

    rxswift: flatten Coordinator subscriptions
    Nested subscriptions are not a good practice in RxSwift. The coordinator
    has now a flat subscriptions chains.

--- RxFlow/Extensions/Rx+Pausable.swift
+++ RxFlow/Extensions/Rx+Pausable.swift
@@ -19,9 +19,39 @@ extension ObservableType {
     ///
     /// - Parameter pauser: The observable sequence used to pause the source observable sequence.
     /// - Returns: The observable sequence which is paused based upon the pauser observable sequence.
-    public func pausable<P: ObservableType> (_ pauser: P) -> Observable<E> where P.E == Bool {
+    public func pausable<P: ObservableType> (withPauser pauser: P) -> Observable<E> where P.E == Bool {
+
         return withLatestFrom(pauser) { element, paused in
-            (element, paused)
+            return (element, paused)
+            }.filter { _, paused in
+                paused
+            }.map { element, _ in
+                element
+        }
+    }
+
+    /// Pauses the elements of the source observable sequence based on the latest element from the second observable sequence.
+    /// The pause is available only after a certain count of events. Before the number of emitted events reaches that count
+    /// the Pauser will not be taken care of. When the Pauser is active, elements are ignored unless the second sequence
+    /// has most recently emitted `true`.
+    /// seealso: [pausable operator on reactivex.io](http://reactivex.io/documentation/operators/backpressure.html)
+    ///
+    /// - Parameter count: the number of events before considering the pauser parameter
+    /// - Parameter pauser: The observable sequence used to pause the source observable sequence.
+    /// - Returns: The observable sequence which is paused based upon the pauser observable sequence.
+    public func pausable<P: ObservableType> (afterCount count: Int, withPauser pauser: P) -> Observable<E> where P.E == Bool {
+
+        return enumerated().flatMap({ (indexedValue) -> Observable<(Self.E, Int)> in
+            let (index, value) = indexedValue
+            return Observable.just((value, index))
+        }).withLatestFrom(pauser) { element, paused in
+            let (value, index) = element
+            if index < count {
+                print ("First step, no pause for value \(value)")
+                return (value, true)
+            }
+            print ("Next step, pause is taken care of for value \(value)")
+            return (value, paused)
             }.filter { _, paused in
                 paused
             }.map { element, _ in
